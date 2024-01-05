3f5fd3ffde22cbaca71b5fabae58d97256e9727a
JW-Ahn0 <ajwquestion@gmail.com>
2024-01-05 17:43:39 +0900

    [FIX] deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 REPOSITORY=/home/ec2-user/riot-api-server
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 chmod +x $JAR_NAME
-nohup /opt/jdk-17/bin/java -jar $JAR_NAME > $REPOSITORY/nohup.out &
+nohup /opt/jdk-17/bin/java -jar $JAR_NAME > $REPOSITORY/nohup.out -Dspring.profiles.active=prod &
