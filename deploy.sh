66b9dad03ec751f1d237d60357e10b52df889d9b
JW-Ahn0 <ajwquestion@gmail.com>
2024-01-05 17:57:26 +0900

    [FIX] deploy.sh 최종 수정

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ echo "> JAR NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
-nohup /opt/jdk-17/bin/java -jar -Dspring.profiles.active=prod $JAR_NAME > $REPOSITORY/nohup.out &
+nohup /opt/jdk-17/bin/java -jar -Dspring.profiles.active=prod $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
