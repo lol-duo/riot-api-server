5b47c9b95a5a9c3dac60fd44810c37da8008f819
JW-Ahn0 <ajwquestion@gmail.com>
2024-01-05 17:23:32 +0900

    [FEAT] CodeDeploy 추가

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/riot-api-server
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+chmod +x $JAR_NAME
+nohup /opt/jdk-17/bin/java -jar $JAR_NAME > $REPOSITORY/nohup.out &
